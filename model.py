029fc97c2d541264134f25289d3d98062ab7ab0f
Phimos <ganyunchong@localhost.localdomain>
2021-08-06 20:49:08 +0800

    [fix] trans matrix default None

--- model.py
+++ model.py
@@ -265,7 +265,7 @@ class CrossEntropyMatrixRegularization(nn.Layer):
         super(CrossEntropyMatrixRegularization, self).__init__()
         self.mat_diff_loss_scale = mat_diff_loss_scale
 
-    def forward(self, pred, target, trans_feat):
+    def forward(self, pred, target, trans_feat=None):
         loss = F.cross_entropy(pred, target)
 
         if trans_feat is None:
